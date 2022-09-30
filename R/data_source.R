9ccb000cd7e52d538b4b332914b2e4df1118fe50
Mitsuo Shiota <mitsuoxv@gmail.com>
2022-09-30 09:05:18 +0900

    change source from NHK to MHLW

--- R/data_source.R
+++ R/data_source.R
@@ -13,8 +13,8 @@ data_source <- function(id) {
     a(href = "https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/",
       "Source: USAFacts")
   } else if (id %in% c("japan", "japan_map")) {
-    a(href = "https://www3.nhk.or.jp/news/special/coronavirus/data/",
-      "Source: NHK")
+    a(href = "https://www.mhlw.go.jp/stf/covid-19/open-data.html",
+      "Source: Ministry of Health, Labour and Welfare")
   } else if (id %in% c("world_region", "world_map", "world_area")) {
     a(href = "https://covid19.who.int/", "Source: WHO")
   } else stop("Incorrect id.")
